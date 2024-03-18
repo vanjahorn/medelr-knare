fd470154f29d4018ef720d87742ee9fd11c2c2ce
Vanja Horneby <vanja.horneby@skola.sala.se>
2024-03-18 10:10:29 +0100

    Add project files.

---
+++ medel/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace medel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double antal = 0;
+            double summa = 0;
+      
+            while (true)
+            {
+                Console.Write("Skriv in ett heltal: ");
+                int tal = int.Parse(Console.ReadLine());
+
+                summa = summa + tal;
+                
+                antal++;
+
+         
+                double medelvärde = summa / antal;
+
+                Console.WriteLine("Medelvärdet är " + medelvärde);
+
+            }
+
+
+        }
+    }
+}
